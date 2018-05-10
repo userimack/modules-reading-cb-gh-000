96fee0e54aa17ed8facf7d1b378567c8c7f415ca
Mahendra Yadav <mahendra.k12@gmail.com>
2018-05-10 17:37:12 +0000

    Done.

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,16 @@
-require_relative './dance_module.rb'
-require_relative './class_methods_module.rb'
+#require_relative './dance_module.rb'
+#require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'
+
+class Kid 
+  #include Dance
+  #extend MetaDancing
+  include FancyDance::InstanceMethods
+  extend FancyDance::ClassMethods
+
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+  end
+end
